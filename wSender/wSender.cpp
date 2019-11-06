38ccaa8f07d73831d1b2d8e0622f7ae4397d724a
jaywang-EE <gaaiia37896@gmail.com>
2019-11-06 16:02:11 -0500

    sender_init

---
+++ wSender/wSender.cpp
@@ -0,0 +1,30 @@
+// Server side C/C++ program to demonstrate Socket programming 
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h> 
+#include <stdlib.h>
+#include <iostream>
+
+#include "session_send.h"
+ 
+using namespace std;
+
+static int err(){
+    printf("Error: missing or extra arguments\n");
+    exit(EXIT_FAILURE);
+}
+
+//<receiver-IP> <receiver-port> <window-size> <input-file> <log>
+int main(int argc, char const *argv[]){
+	if(argc != 6) err();
+	int port, win_size;
+	char const *host, *input_path, *log_path;
+	host = argv[1];
+	port = atoi(argv[2]);
+	win_size = atoi(argv[3]);
+	input_path = argv[4];
+	log_path = argv[5];
+	WSender sender(host, port, win_size, log_path);
+	sender.send(input_path);
+	return 0;
+}
