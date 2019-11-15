1bfcde6117273d7d42eadfc14bc2ece421e50e18
WeichenZh <54957828+WeichenZh@users.noreply.github.com>
2019-11-15 12:19:14 -0500

    Update wReceiver.cpp

--- WTP-base/wReceiver.cpp
+++ WTP-base/wReceiver.cpp
@@ -23,14 +23,6 @@ int main(int argc, char const *argv[])
 	win_size = atoi(argv[2]);
 	output_dir = argv[3];
 	log_path = argv[4];
-
-	
-	string outFilePath = string(output_dir) + "/FILE-0.out";
-	ofstream output_file;
-
-	cout << "first create output file:"  << outFilePath <<endl;
-	output_file.open(outFilePath.c_str(), ios::trunc);
-	output_file.close();
 	
 	WReceiver receiver(port, win_size, output_dir, log_path);
 	while(1)
